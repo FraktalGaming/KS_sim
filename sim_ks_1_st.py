d6bb45840bb713e867d5df8231e9cbc835bd03a2
FraktalGaming <fraktal0gaming@gmail.com>
2025-11-18 17:51:36 +0100

    2nd push

--- sim_ks_1_st.py
+++ sim_ks_1_st.py
@@ -1,13 +1,355 @@
 import streamlit as st
+
+
 import numpy as np
+import time
 
-st.write('toto')
-
-
-inf_att_p1 = st.number_input('Infantry attack (in %)', min_value=0.0, value=200.0, step=None, format="%0.2f", key=None, help=None, on_change=None, args=None, kwargs=None)
-def fc_titi():
-    st.write(inf_att_p1)
+import matplotlib.pyplot as plt
+import sim_ks_lib as sksl
 
 
-st.button('titi', on_click=fc_titi)
+st.sidebar.title("Stats")
+with st.sidebar.form("toto"):  
+    st.write('Yours stats')
+    inf_troops_p1 = st.number_input(
+        'Initial infantry troops',
+        min_value=0,
+        value=10000,
+        step=None,
+        format="%d", key='inf_troops_p1')
+    cav_troops_p1 = st.number_input(
+        'Initial cavalry troops',
+        min_value=0,
+        value=10000,
+        step=None,
+        format="%d", key='cav_troops_p1')
+    arc_troops_p1 = st.number_input(
+        'Initial archery troops',
+        min_value=0,
+        value=10000,
+        step=None,
+        format="%d", key='arc_troops_p1')
+
+    inf_att_p1 = st.number_input(
+        'Infantry attack (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='inf_att_p1')
+
+    inf_def_p1 = st.number_input(
+        'Infantry defense (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='inf_def_p1')
+
+    inf_let_p1 = st.number_input(
+        'Infantry lethality (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='inf_let_p1')
+
+    inf_hea_p1 = st.number_input(
+        'Infantry health (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='inf_hea_p1')
+   
+        
+    cav_att_p1 = st.number_input(
+        'Cavalry attack (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='cav_att_p1')
+
+    cav_def_p1 = st.number_input(
+        'Cavalry defense (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='cav_def_p1')
+
+    cav_let_p1 = st.number_input(
+        'Cavalry lethality (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='cav_let_p1')
+
+    cav_hea_p1 = st.number_input(
+        'Cavalry health (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='cav_hea_p1')
+   
+    arc_att_p1 = st.number_input(
+        'Archery attack (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='arc_att_p1')
+
+    arc_def_p1 = st.number_input(
+        'Archery defense (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='arc_def_p1')
+
+    arc_let_p1 = st.number_input(
+        'Archery lethality (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='arc_let_p1')
+
+    arc_hea_p1 = st.number_input(
+        'Archery health (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f", key='arc_hea_p1')
+   
+
+    st.write('Opponents stats')
+    inf_troops_p2 = st.number_input(
+        'Initial infantry troops',
+        min_value=0,
+        value=10000,
+        step=None,
+        format="%d")
+    cav_troops_p2 = st.number_input(
+        'Initial cavalry troops',
+        min_value=0,
+        value=10000,
+        step=None,
+        format="%d")
+    arc_troops_p2 = st.number_input(
+        'Initial archery troops',
+        min_value=0,
+        value=10000,
+        step=None,
+        format="%d")
+
+    inf_att_p2 = st.number_input(
+        'Infantry attack (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+
+    inf_def_p2 = st.number_input(
+        'Infantry defense (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+
+    inf_let_p2 = st.number_input(
+        'Infantry lethality (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+
+    inf_hea_p2 = st.number_input(
+        'Infantry health (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+   
+        
+    cav_att_p2 = st.number_input(
+        'Cavalry attack (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+
+    cav_def_p2 = st.number_input(
+        'Cavalry defense (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+
+    cav_let_p2 = st.number_input(
+        'Cavalry lethality (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+
+    cav_hea_p2 = st.number_input(
+        'Cavalry health (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+   
+    arc_att_p2 = st.number_input(
+        'Archery attack (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+
+    arc_def_p2 = st.number_input(
+        'Archery defense (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+
+    arc_let_p2 = st.number_input(
+        'Archery lethality (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+
+    arc_hea_p2 = st.number_input(
+        'Archery health (in %)',
+        min_value=0.0,
+        value=200.0,
+        step=None,
+        format="%0.2f")
+   
+
+    submitted = st.form_submit_button("Create plots!")
+
+
+st.write('Modify the values on the left, and click the button!')
+
+
+#inf_att_p1 = st.number_input('Infantry attack (in %)', min_value=0.0, value=200.0, step=None, format="%0.2f", key=None, help=None, on_change=None, args=None, kwargs=None)
+#def fc_titi():
+#    st.write(inf_att_p1)
+
+
+#button = st.button('titi', on_click=fc_titi)
+
+
+
+p1_stats = {
+    "init_troops": [inf_troops_p1, cav_troops_p1, arc_troops_p1],
+    "inf_attack": inf_att_p1,
+    "inf_defense": inf_def_p1,
+    "inf_lethality": inf_let_p1,
+    "inf_health": inf_hea_p1,
+    "cav_attack": cav_att_p1,
+    "cav_defense": cav_def_p1,
+    "cav_lethality": cav_let_p1,
+    "cav_health": cav_hea_p1,
+    "arc_attack": arc_att_p1,
+    "arc_defense": arc_def_p1,
+    "arc_lethality": arc_let_p1,
+    "arc_health": arc_hea_p1,
+    "player_name": "Your name",
+    "battle_name": ""
+}
+
+
+
+
+p2_stats = {
+    "init_troops": [inf_troops_p2, cav_troops_p2, arc_troops_p2],
+    "inf_attack": inf_att_p2,
+    "inf_defense": inf_def_p2,
+    "inf_lethality": inf_let_p2,
+    "inf_health": inf_hea_p2,
+    "cav_attack": cav_att_p2,
+    "cav_defense": cav_def_p2,
+    "cav_lethality": cav_let_p2,
+    "cav_health": cav_hea_p2,
+    "arc_attack": arc_att_p2,
+    "arc_defense": arc_def_p2,
+    "arc_lethality": arc_let_p2,
+    "arc_health": arc_hea_p2,
+    "player_name": "Your name",
+    "battle_name": ""
+}
+
+
+if submitted:
+
+    player1 = sksl.Fighter(p1_stats)
+    player2 = sksl.Fighter(p2_stats)
+
+
+    n_battles = 10
+    step = 0.05
+    f_inf_min = 0.1
+
+    ta = time.time()
+    finf_tab, fcav_tab, farc_tab, res_tab = sksl.compute_win_chances(player1, player2, n_battles, step, f_inf_min=f_inf_min)
+    print(time.time()-ta)
+
+    id_best = np.where(np.array(res_tab) == np.array(res_tab).max())[0][0]
+    print(finf_tab[id_best])
+    print(fcav_tab[id_best])
+    print(farc_tab[id_best])
+    finf_best = np.round(finf_tab[id_best], 2)
+    fcav_best = np.round(fcav_tab[id_best], 2)
+    farc_best = np.round(farc_tab[id_best], 2)
+
+
+    min_val = np.array(res_tab).max()
+    if min_val < 20:
+        vmin = 20
+    else:
+        vmin = 100
+
+
+    fig1 = plt.figure(2)
+    ax = fig1.gca()
+
+    sc = ax.scatter(finf_tab, fcav_tab, c=np.array(res_tab)*100, vmin=0)
+    ax.plot(.50, .25, c='r', lw=0, marker='x', ms=5, label='50/25/25', mew=3)
+    ax.plot(finf_best, fcav_best,  c='m', lw=0, marker='x', ms=3, label='Best {}/{}/{}'.format(int(finf_best*100), int(fcav_best*100), int(farc_best*100)), mew=3)
+    ax.set_xlabel('f_inf')
+    ax.set_ylabel('f_cav')
+    ax.set_xlim(-0.05, 1.05)
+    ax.set_ylim(-0.05, 1.05)
+    ax.legend()
+    ax.set_title('inf - cav ratio for {}, {}'.format(player1.player_name, player1.battle_name)) 
+    fig1.colorbar(sc, label='chances of victory [%] ')
+
+
+    # fig2 = plt.figure(3)
+    # plt.clf()
+    # plt.scatter(farc_tab, finf_tab, c=np.array(res_tab)*100, vmin=0)
+    # plt.plot(.25, .50, c='r', lw=0, marker='x', ms=5, label='50/25/25', mew=3)
+    # plt.plot(farc_best, finf_best,  c='m', lw=0, marker='x', ms=3, label='Best {}/{}/{}'.format(int(finf_best*100), int(fcav_best*100), int(farc_best*100)), mew=3)
+    # plt.xlabel('f_arc')
+    # plt.ylabel('f_inf')
+    # plt.xlim(-0.05, 1.05)
+    # plt.ylim(-0.05, 1.05)
+    # plt.legend()
+    # plt.title('arc - inf ratio for {}, {}'.format(player1.player_name, player1.battle_name)) 
+    # plt.colorbar(label='chances of victory [%] ')
+    # plt.title('arc - inf ratio for {}, {}'.format(player1.player_name, player1.battle_name))
+
+
+
+    st.pyplot(fig1)
+    # st.pyplot(fig2)
+
+            # fig = plt.figure(1, figsize=[fs/1.2 for fs in matplotlib.rcParams['figure.figsize']])
+            # ax = fig.gca()
+            # ax.errorbar(XX[:, 1], YY, yerr=sigma, fmt='k.', label='Training data')
+            # ax.plot(x_2[:,1], x_2 @ w_ols, 'r-', label='OLS')
+            # ax.plot(x_, alpha*x_ + b, 'b-', label='model')
+            # ax.set_xlim(x_min, x_max)
+            # ax.set_ylim(alpha *x_min + b-2*sigma, alpha *x_max + b + 2*sigma)
+            # ax.set_xlabel('x', fontsize=14)
+            # ax.set_ylabel('y', fontsize=14)
+            # ax.legend()
+            # st.pyplot(fig)
 
---
+++ sim_ks_lib.py
@@ -0,0 +1,353 @@
+
+import numpy as np
+import time
+
+troop_tier = 't10'
+verbose = False
+stats_t6 = [
+    [243, 730],
+    [730, 243],
+    [974, 183]
+]
+
+
+stats_t10 = [
+    [472, 1416],
+    [1416, 472],
+    [1888, 354]
+]
+
+stats_tg1 = [
+    [491, 1473],
+    [1473, 491],
+    [1904, 388]
+]
+stats_tg2 = [
+    [515, 1546],
+    [1546, 515],
+    [2062, 387]
+]
+stats_tg3 = [
+    [541, 1624],
+    [1624, 541],
+    [2165, 406]
+]
+
+
+class Fighter:
+
+    def __init__(self, init_dict):
+
+        self.init_dict = init_dict
+    
+        self.load_dict()
+
+        self.load_tier()
+
+        self.current_troops = self.init_troops
+
+    def load_dict(self):
+        
+        d = self.init_dict
+        self.init_troops = d['init_troops']
+        self.inf_attack = d['inf_attack']
+        self.inf_defense = d['inf_defense']
+        self.inf_lethality = d['inf_lethality']
+        self.inf_health = d['inf_health']
+        self.cav_attack = d['cav_attack']
+        self.cav_defense = d['cav_defense']
+        self.cav_lethality = d['cav_lethality']
+        self.cav_health = d['cav_health']
+        self.arc_attack = d['arc_attack']
+        self.arc_defense = d['arc_defense']
+        self.arc_lethality = d['arc_lethality']
+        self.arc_health = d['arc_health']
+        self.init_total_troops = np.sum(np.array(d['init_troops']))
+
+        if "player_name" in d:
+            self.player_name = d["player_name"]
+        else:
+            self.player_name = ''
+
+        if "battle_name" in d:
+            self.battle_name = d["battle_name"]
+        else:
+            self.battle_name = ''
+        if "troop_tier" in d:
+            self.troop_tier = d['troop_tier']
+        else:
+            self.troop_tier = 't10'
+
+    def load_tier(self):
+
+        if self.troop_tier == 't6':
+            print('t6 troop!')
+            self.units_stats = stats_t6
+
+        if self.troop_tier == 't10':
+            print('t10 troop!')
+            self.units_stats = stats_t10
+
+        if self.troop_tier == 'tg1':
+            print('tg1 troop!')
+            self.units_stats = stats_tg1
+
+        if self.troop_tier == 'tg2':
+            print('tg2 troop!')
+            self.units_stats = stats_tg2
+
+        if self.troop_tier == 'tg3':
+            print('tg3 troop!')
+            self.units_stats = stats_tg3
+
+
+def get_dead(fighter, opponent, fighter_unittype, opponent_unittype):
+
+
+    nfighter_0 = fighter.init_total_troops
+    nopponent_0 = opponent.init_total_troops
+
+    new_number = np.sqrt(np.min( [nfighter_0, nopponent_0])) # fighter.toto# 1 # 14.14 #nfighter / np.max([nfighter, nopponent ])
+
+    units_statsf = fighter.units_stats
+    units_statso = opponent.units_stats
+    if fighter_unittype == 'inf':
+        attack_factor = np.sqrt(fighter.current_troops[0]) * 10 * units_statsf[0][0] * (1 + fighter.inf_attack/100) *(1+fighter.inf_lethality/100) / 100 *new_number
+    elif fighter_unittype == 'cav':
+        attack_factor = np.sqrt(fighter.current_troops[1]) * 10 * units_statsf[1][0] * (1 + fighter.cav_attack/100) *(1+fighter.cav_lethality/100) / 100 *new_number
+    elif fighter_unittype == 'arc':
+        attack_factor = np.sqrt(fighter.current_troops[2]) * 10 * units_statsf[2][0] * (1 + fighter.arc_attack/100) *(1+fighter.arc_lethality/100) / 100 *new_number
+   
+
+    if opponent_unittype == 'inf':
+        defense_factor = 10 * units_statso[0][1] * (1 + opponent.inf_defense/100) *(1+opponent.inf_health/100)# / 100
+    elif opponent_unittype == 'cav':
+        defense_factor = 10 * units_statso[1][1] * (1 + opponent.cav_defense/100) *(1+opponent.cav_health/100) #/ 100
+    elif opponent_unittype == 'arc':
+        defense_factor = 10 * units_statso[2][1] * (1 + opponent.arc_defense/100) *(1+opponent.arc_health/100)# / 100
+   
+    att_fac_bonus = 0
+    def_fac_bonus = 0
+    if (fighter_unittype == 'inf') and (opponent_unittype == 'cav'):
+        #attack_factor *= 1.1
+        att_fac_bonus += 0.1
+    if (fighter_unittype == 'cav') and (opponent_unittype == 'inf'):
+        #defense_factor *= 1.1
+        def_fac_bonus += 0.1
+    if (fighter_unittype == 'cav') and (opponent_unittype == 'arc'):
+        #attack_factor *= 1.1
+        att_fac_bonus += 0.1
+    if (fighter_unittype == 'arc') and (opponent_unittype == 'inf'):
+        #attack_factor *= 1.1
+        att_fac_bonus += 0.1
+
+    if fighter.troop_tier == 'tg3':
+        if fighter_unittype == 'arc':
+            att_fac_bonus += 0.5*((np.random.rand(1) > 0.8)[0])
+        if fighter_unittype == 'cav':
+            att_fac_bonus += 1*((np.random.rand(1) > 0.9)[0])
+        if opponent_unittype == 'inf':
+            def_fac_bonus = 0.36*((np.random.rand(1) > 0.75)[0])
+
+    att_fac_bonus += 1
+    def_fac_bonus += 1
+
+    deads = attack_factor*att_fac_bonus/(defense_factor * def_fac_bonus)
+    return deads
+
+
+def round(fighter, opponent):
+
+    fighter_current_troops = fighter.current_troops
+    opponent_current_troops = opponent.current_troops
+
+    if fighter_current_troops[0]> 0:
+        if verbose:
+            print('fighter has some infantry')
+        if opponent_current_troops[0] - 0* opponent.dead_this_round[0]  > 0:
+            if verbose:
+                print('opponent has some infantry. inf1 vs inf2')
+            dead = get_dead(fighter, opponent, 'inf', 'inf')
+            opponent.dead_this_round[0] += dead
+
+        elif opponent_current_troops[1] - 0*opponent.dead_this_round[1]  > 0:
+            if verbose:
+                print('opponent has no infantry, but some cavalry. inf1 vs cav2')
+            dead = get_dead(fighter, opponent, 'inf', 'cav')
+            opponent.dead_this_round[1] += dead
+
+        else:
+            if verbose:
+                print('opponent has no cavalry, but some arcehry. inf1 vs arc2')
+            dead = get_dead(fighter, opponent, 'inf', 'arc')
+            opponent.dead_this_round[2] += dead
+    else:
+        if verbose:
+            print('fighter has no infantry')
+
+    if fighter_current_troops[1] > 0:
+        if verbose:
+            print('fighter has some cavalry')
+        cav_ambush = (np.random.rand(1) > 0.8)[0]
+        if cav_ambush:
+            if verbose:
+                print('cav do ambush!!')
+            if opponent_current_troops[2] - 0*opponent.dead_this_round[2] > 0:
+                if verbose:
+                    print('opponent has arch, cav1 vs arc2')
+                dead = get_dead(fighter, opponent, 'cav', 'arc')
+                opponent.dead_this_round[2] += dead
+                ## do the cav1 vs arc2
+        else:
+            if opponent_current_troops[0] - 0*opponent.dead_this_round[0]  > 0:
+                if verbose:
+                    print('opponent has some infantry. cav1 vs inf2')
+                dead = get_dead(fighter, opponent, 'cav', 'inf')
+                opponent.dead_this_round[0] += dead
+            elif opponent_current_troops[1] - 0*opponent.dead_this_round[1] > 0:
+                if verbose:
+                    print('opponent has no infantry, but some cavalry. cav1 vs cav2')
+                dead = get_dead(fighter, opponent, 'cav', 'cav')
+                opponent.dead_this_round[1] += dead
+            else:
+                if verbose:
+                    print('opponent has no cavalry, but some arcehry. cav1 vs arc2')
+                dead = get_dead(fighter, opponent, 'cav', 'arc')
+                opponent.dead_this_round[2] += dead
+    else: 
+        if verbose:
+            print('fighter has no cav')
+
+    if fighter_current_troops[2] > 0:
+        if verbose:
+            print('fighter has some archery')
+        arc_volley = (np.random.rand(1) > 0.9)[0]
+        if opponent_current_troops[0] - 0*opponent.dead_this_round[0] > 0:
+            if verbose:
+                print('opponent has some infantry. arc1 vs inf2')
+            dead = get_dead(fighter, opponent, 'arc', 'inf')
+            opponent.dead_this_round[0] += dead
+        elif opponent_current_troops[1] - 0*opponent.dead_this_round[1] > 0:
+            if verbose:
+                print('opponent has no infantry, but some cavalry. arc1 vs cav2')
+            dead = get_dead(fighter, opponent, 'arc', 'cav')
+            opponent.dead_this_round[1] += dead
+        else:
+            if verbose:
+                print('opponent has no cavalry, but some arcehry. arc1 vs arc2')
+            dead = get_dead(fighter, opponent, 'arc', 'arc')
+            opponent.dead_this_round[2] += dead
+        if arc_volley:
+            if verbose:
+                print('Arch volley!')
+            if opponent_current_troops[0] - 0*opponent.dead_this_round[0]  > 0:
+                if verbose:
+                    print('opponent has some infantry. arc1 vs inf2')
+                dead = get_dead(fighter, opponent, 'arc', 'inf')
+                opponent.dead_this_round[0] += dead
+            elif opponent_current_troops[1] - 0*opponent.dead_this_round[1] > 0:
+                if verbose:
+                    print('opponent has no infantry, but some cavalry. arc1 vs cav2')
+                dead = get_dead(fighter, opponent, 'arc', 'cav')
+                opponent.dead_this_round[1] += dead
+            else:
+                if verbose:
+                    print('opponent has no cavalry, but some arcehry. arc1 vs arc2')
+                dead = get_dead(fighter, opponent, 'arc', 'arc')
+                opponent.dead_this_round[1] += dead
+
+
+def get_results(p_1, p_2):
+
+    p_1.current_troops = p_1.init_troops.copy()
+    p_2.current_troops = p_2.init_troops.copy()
+    #print(p_1.current_troops)
+    #print(p_2.current_troops)
+
+    t0 = time.time()
+
+    cont = True
+    nb_round = 0
+    while cont:
+        nb_round += 1
+        #if verbose:
+        #    print('Round {}'.format(nb_round))
+
+        p_1.dead_this_round = [0, 0, 0]
+        p_2.dead_this_round = [0, 0, 0]
+        round(p_1, p_2)
+        round(p_2, p_1)
+
+        ## update troops:
+        p_1.current_troops[0] -= p_1.dead_this_round[0]
+        p_1.current_troops[1] -= p_1.dead_this_round[1]
+        p_1.current_troops[2] -= p_1.dead_this_round[2]
+
+        p_2.current_troops[0] -= p_2.dead_this_round[0]
+        p_2.current_troops[1] -= p_2.dead_this_round[1]
+        p_2.current_troops[2] -= p_2.dead_this_round[2]
+        #if verbose:
+        #    print('-----------------------')
+        #    print(p_1.current_troops, p_2.current_troops)
+        #    print('-----------------------')
+        if (np.clip(p_1.current_troops, 0, 10000000000).sum()<= 0) or (np.clip(p_2.current_troops, 0,10000000000).sum()<= 0):
+            cont = False
+
+    if verbose:
+        print('-----------------------')
+        print('End of battle')
+        print('P1 has ', np.round(np.clip(p_1.current_troops, 0, 10000000000)))
+        print('P2 has ', np.round(np.clip(p_2.current_troops, 0, 10000000000)))
+        print('-----------------------')
+
+    end_troops_p1 = np.round(np.clip(p_1.current_troops, 0, 10000000000))
+    end_troops_p2 = np.round(np.clip(p_2.current_troops, 0, 10000000000))
+
+    is_victory = end_troops_p1.sum() > end_troops_p2.sum()
+    t1 = time.time()
+    #print(t1-t0)
+    return end_troops_p1, end_troops_p2, is_victory
+
+
+def get_average_results(N, player1_, player2_):
+    vic_tab = np.zeros(N) 
+    for i in range(N):
+        _, _, vic_tab[i] = get_results(player1_, player2_)
+
+    return sum(vic_tab)/N
+
+
+def compute_win_chances(player1_, player2_, n_battles, step=0.05, f_inf_min=0.40):
+    t0 = time.time()
+    N_total = np.array(player1_.init_dict['init_troops']).sum()
+    print(N_total)
+    finf_tab = []
+    fcav_tab = []
+    farc_tab = []
+    res_tab = []
+    k = 0
+    for f_inf in np.arange(f_inf_min, 1.0001, step):
+        for f_cav in np.arange(0, np.min([1.003 - f_inf, 0.801]), step):
+            f_arc = 1 - f_inf - f_cav
+            N_inf = int(N_total * f_inf)
+            N_cav = int(N_total * f_cav)
+            N_arc = int(N_total * f_arc)
+
+            player1_.init_troops = [N_inf, N_cav, N_arc]
+
+            player1_.current_troops = player1_.init_troops.copy()
+            player2_.current_troops = player2_.init_troops.copy()
+            t0_1 = time.time()
+            avg = get_average_results(n_battles, player1_, player2_)
+            t0_2 = time.time()
+            k += t0_2-t0_1
+
+            finf_tab.append(f_inf)
+            fcav_tab.append(f_cav)
+            farc_tab.append(f_arc)
+            res_tab.append(avg)
+
+    t1 = time.time()
+
+    print(k)
+    return finf_tab, fcav_tab, farc_tab, res_tab
